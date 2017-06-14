fd444d823c1b0b9b92af97ab413bb33256899123
eugene-gilmore <eugene.gilmore@griffithuni.edu.au>
2017-06-15 00:49:25 +1000

    Point to my fork of libui module map

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "ui",
     dependencies: [
-        .Package(url: "https://github.com/sclukey/clibui.git", majorVersion:1),
+        .Package(url: "https://github.com/eugene-gilmore/clibui.git", majorVersion:1),
     ]
 )
