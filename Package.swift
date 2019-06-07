e7ee40217cc304c53b6fc12dc4f3e55f5d046274
Eugene Gilmore <eugene.gilmore@griffithuni.edu.au>
2019-06-08 00:34:35 +1000

    add product

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "ui",
+    products: [
+	.library(name: "ui", targets: ["ui"]),
+    ],
     dependencies: [
         .package(url: "/Users/eugene/src/clibui", .branch("master")),
         //.package(url: "https://github.com/eugene-gilmore/clibui.git", from : "1.0.0"),
